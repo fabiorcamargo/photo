649a8b1a0a13031489061f362a5592e4a316c2d0
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-16 20:35:12 -0300

    up

--- resources/views/livewire/gallery.blade.php
+++ resources/views/livewire/gallery.blade.php
@@ -3,57 +3,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3>
-                    <i class="fa fa-camera-retro heading-icon" aria-hidden="true"></i>
-                    Simple Image Gallery
-                </h3>
-
+                @foreach ($gallery as $key => $value)
                 <ul class="box-container three-cols">
                     <li class="box">
                         <div class="inner">
                             <a href="demo/img/large/gm1.jpg" class="glightbox">
-                                <img src="demo/img/small/gm1.jpg" alt="image" />
-                            </a>
-                        </div>
-                    </li>
-                    <li class="box">
-                        <div class="inner">
-                            <a href="demo/img/large/gm2.jpg" class="glightbox">
-                                <img src="demo/img/small/gm2.jpg" alt="image" />
-                            </a>
-                        </div>
-                    </li>
-                    <li class="box">
-                        <div class="inner">
-                            <a href="demo/img/large/gm3.jpg" class="glightbox">
-                                <img src="demo/img/small/gm3.jpg" alt="image" />
-                            </a>
-                        </div>
-                    </li>
-                    <li class="box">
-                        <div class="inner">
-                            <a href="demo/img/large/gm4.jpg" class="glightbox">
-                                <img src="demo/img/small/gm4.jpg" alt="image" />
-                            </a>
-                        </div>
-                    </li>
-                    <li class="box">
-                        <div class="inner">
-                            <a href="demo/img/large/gm5.jpg" class="glightbox">
-                                <img src="demo/img/small/gm5.jpg" alt="image" />
-                            </a>
-                        </div>
-                    </li>
-                    <li class="box">
-                        <div class="inner">
-                            <a href="demo/img/large/gm6.jpg" class="glightbox">
-                                <img src="demo/img/small/gm6.jpg" alt="image" />
+                                <img src="{{ $value->photos->first()->getFirstMediaUrl('portifolio-images', 'thumb') }}" alt="image" />
                             </a>
                         </div>
                     </li>
                 </ul>
+                @endforeach
             </div>
         </div>
     </div>
 </section>
-
