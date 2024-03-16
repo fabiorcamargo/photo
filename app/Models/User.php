774539b2cd46a32c6eb71915f138c20b7a36a502
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-16 16:33:29 -0300

    up

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -42,4 +46,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '*') && $this->hasVerifiedEmail();
+    }
 }
