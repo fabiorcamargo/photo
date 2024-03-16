d141c9ffb340a7e5a6e69f609f910cd087dd9fa1
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-16 20:33:58 -0300

    up

--- resources/views/livewire/gallery.blade.php
+++ resources/views/livewire/gallery.blade.php
@@ -1,16 +1,59 @@
 
-<div class="px-4">
-    <ul class="box-container grid gap-3 columns-2 md:columns-3 relative" style="flex-wrap: nowrap;">
-        @foreach ($gallery as $key => $value)
-        <li class="box">
-            <!-- Botão para abrir o modal -->
-            <div class="inner">
-                <a href="/portifolio/{{$value->id}}" class="glightbox">
-                    <img class="imagem-vibrante" src="{{ $value->photos->first()->getFirstMediaUrl('portifolio-images', 'thumb') }}" />
-                </a>
+<section id="examples" class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>
+                    <i class="fa fa-camera-retro heading-icon" aria-hidden="true"></i>
+                    Simple Image Gallery
+                </h3>
+
+                <ul class="box-container three-cols">
+                    <li class="box">
+                        <div class="inner">
+                            <a href="demo/img/large/gm1.jpg" class="glightbox">
+                                <img src="demo/img/small/gm1.jpg" alt="image" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="box">
+                        <div class="inner">
+                            <a href="demo/img/large/gm2.jpg" class="glightbox">
+                                <img src="demo/img/small/gm2.jpg" alt="image" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="box">
+                        <div class="inner">
+                            <a href="demo/img/large/gm3.jpg" class="glightbox">
+                                <img src="demo/img/small/gm3.jpg" alt="image" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="box">
+                        <div class="inner">
+                            <a href="demo/img/large/gm4.jpg" class="glightbox">
+                                <img src="demo/img/small/gm4.jpg" alt="image" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="box">
+                        <div class="inner">
+                            <a href="demo/img/large/gm5.jpg" class="glightbox">
+                                <img src="demo/img/small/gm5.jpg" alt="image" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="box">
+                        <div class="inner">
+                            <a href="demo/img/large/gm6.jpg" class="glightbox">
+                                <img src="demo/img/small/gm6.jpg" alt="image" />
+                            </a>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        </li>
-        @endforeach
-    </ul>
-</div>
+        </div>
+    </div>
+</section>
 
