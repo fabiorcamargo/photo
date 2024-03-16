3d30a67cd44d7d45172a9192cde4ece0b427e9ef
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-16 20:41:44 -0300

    up

--- resources/views/livewire/gallery.blade.php
+++ resources/views/livewire/gallery.blade.php
@@ -1,19 +1,23 @@
 <section id="examples" class="section">
     <div class="container">
         <div class="row">
-            <ul class="box-container d-md-block">
-                @foreach ($gallery as $key => $value)
-                <li class="box">
-                    <!-- Botão para abrir o modal -->
-                    <div class="inner">
-                        <a href="/portifolio/{{$value->id}}" class="glightbox">
-                            <img class="imagem-vibrante"
-                                src="{{ $value->photos->first()->getFirstMediaUrl('portifolio-images', 'thumb') }}" />
-                        </a>
-                    </div>
-                </li>
-                @endforeach
-            </ul>
+            <div class="px-4">
+                <ul class="box-container grid gap-3 columns-2 md:columns-3 relative">
+                    @foreach ($gallery as $key => $value)
+
+                    <li class="box">
+                        <!-- Botão para abrir o modal -->
+                        <div class="inner">
+                            <a href="/portifolio/{{$value->id}}" class="glightbox">
+                                <img class="imagem-vibrante"
+                                    src="{{ $value->photos->first()->getFirstMediaUrl('portifolio-images', 'thumb') }}" />
+                            </a>
+                        </div>
+                    </li>
+
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
 </section>
