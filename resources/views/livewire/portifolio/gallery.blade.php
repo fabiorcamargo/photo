248d8901d71503bf10640b575e442d24fa8d8cc3
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-16 20:20:36 -0300

    up

--- resources/views/livewire/portifolio/gallery.blade.php
+++ resources/views/livewire/portifolio/gallery.blade.php
@@ -1,16 +1,18 @@
 <div>
-    <div class="px-4">
-        <ul class="box-container columns-2 md:columns-3 relative" style="display: flex; flex-wrap: wrap;">
-            @foreach ($photos->getMedia('portifolio-images') as $value)
-            <li class="box">
-                <!-- Botão para abrir o modal -->
-                <div class="inner">
-                    <a href="{{$value->getUrl()}}" class="glightbox">
-                        <img class="imagem-vibrante" src="{{$value->getUrl('thumb')}}" />
-                    </a>
-                </div>
-            </li>
-            @endforeach
-        </ul>
+    <div>
+        <div class="px-4">
+            <ul class="box-container columns-2 relative" style="display: flex; flex-wrap: wrap;">
+                @foreach ($photos->getMedia('portifolio-images') as $value)
+                <li class="box">
+                    <!-- Botão para abrir o modal -->
+                    <div class="inner">
+                        <a href="{{$value->getUrl()}}" class="glightbox">
+                            <img class="imagem-vibrante" src="{{$value->getUrl('thumb')}}" />
+                        </a>
+                    </div>
+                </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 </div>
