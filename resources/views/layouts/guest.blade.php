a67b3e22efa62ac8ed811ebd82d525169e73e057
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-16 17:41:39 -0300

    up

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,12 +17,13 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
     <body class="font-sans bg-white antialiased">
         {{ $slot }}
 
 
-
+        @livewireScripts
     </body>
 
 
