dd2bd4d836612b28435db063a11960e8204426b7
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-17 15:45:53 -0300

    up

--- resources/views/livewire/gallery.blade.php
+++ resources/views/livewire/gallery.blade.php
@@ -1,12 +1,12 @@
 <div>
-    <div class="px-4">
+    {{-- <div class="px-4">
         <ul class="box-container grid gap-3 columns-2 md:columns-3 relative">
             @foreach ($gallery as $key => $value)
 
             <li class="box">
                 <!-- Botão para abrir o modal -->
                 <div class="inner">
-                    <a href="/portifolio/{{$value->id}}" class="glightbox">
+                    <a href="/portifolio/{{$value->id}}" class="glightbox rounded-sm ">
                         <img class="imagem-vibrante"
                             src="{{ $value->photos->first()->getFirstMediaUrl('portifolio-images', 'thumb') }}" />
                     </a>
@@ -15,5 +15,26 @@
 
             @endforeach
         </ul>
-    </div>
+    </div> --}}
+
+    <!-- Simple images example -->
+    <section >
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="box-container three-cols">
+                        @foreach ($gallery as $key => $value)
+                        <li class="box">
+                            <div class="inner">
+                                <a href="demo/img/large/gm1.jpg" class="glightbox">
+                                    <img src="{{ $value->photos->first()->getFirstMediaUrl('portifolio-images', 'thumb') }}" alt="image" />
+                                </a>
+                            </div>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
