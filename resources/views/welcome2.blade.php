503431e1d8a992931947c919a7366cff646548f5
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-19 10:58:05 -0300

    up

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    <link rel="stylesheet" href="{{asset('plugins/glightbox/demo/css/style.css')}}" />
-    <link rel="stylesheet" href="{{asset('plugins/glightbox/dist/css/glightbox.css')}}" />
+    <link  href="{{asset('plugins/glightbox/demo/css/style.css')}}" />
+    <link  href="{{asset('plugins/glightbox/dist/css/glightbox.css')}}" />
     <style>
         .imagem-vibrante {
             filter: saturate(120%);
@@ -15,12 +15,8 @@
     <livewire:testimonial />
 
 
-
-
-
-
     <script src="{{asset('plugins/glightbox/demo/js/valde.min.js')}}"></script>
-    <script src="{{asset('plugins/glightbox/dist/js/glightbox.js')}}"></script>
+    <script src="{{asset('plugins/glightbox/dist/js/glightbox.min.js')}}"></script>
     <script src="{{asset('plugins/glightbox/demo/js/site.js')}}"></script>
 
 </x-guest-layout>
