bb254075f5dde2e9f5fb31185a4ff3e88328b91c
Fabio Camargo <fabiorcamargo@gmail.com>
2024-03-19 21:04:07 -0300

    up

--- resources/views/livewire/portifolio/portifolio-show.blade.php
+++ resources/views/livewire/portifolio/portifolio-show.blade.php
@@ -1,8 +1,6 @@
 <div>
 
-    <link rel="stylesheet" href="{{asset('plugins/glightbox/demo/css/style.css')}}" />
     <link rel="stylesheet" href="{{asset('plugins/glightbox/dist/css/glightbox.css')}}" />
-    <link rel="stylesheet" href="{{asset('plugins/simplelightbox/dist/simple-lightbox.css')}}" />
     <style>
         .imagem-vibrante {
             filter: saturate(120%);
@@ -17,9 +15,7 @@
 
     <livewire:portifolio.gallery :portifolio="$portifolio"/>
 
-    <script src="{{asset('plugins/glightbox/demo/js/valde.min.js')}}"></script>
         <script src="{{asset('plugins/glightbox/dist/js/glightbox.js')}}"></script>
-        <script src="{{asset('plugins/glightbox/demo/js/site.js')}}"></script>
         <script>
             var lightbox = GLightbox();
             lightbox.on('open', (target) => {
